bfce690162d13414b9e946acea82b9bd86a6f722
nathidaum <nathidaum@gmail.com>
2024-10-12 16:38:41 +0200

    Added functionality for the player to move up

--- js/main.js
+++ js/main.js
@@ -1,46 +1,65 @@
+/***************************************************/
+/******************** PLAYER ***********************/
+/***************************************************/
+
 class Player {
     constructor() {
-        this.height = 15; 
-        this.width = 10; 
-        this.positionX = 50 - this.width / 2; // centered starting position
+        this.height = 15;
+        this.width = 10;
+        this.positionX = 50 - this.width / 2; // Centered starting position
         this.positionY = 0;
-        this.platformElement = null; 
+        this.playerElement = null;
 
         this.createPlayer();
     }
-    
+
     createPlayer() {
-        this.platformElement = document.createElement("div");
-        console.log("player element created");
+        this.playerElement = document.createElement("div");
 
-        this.platformElement.id = "player"; // add ID
+        this.playerElement.id = "player"; // Add ID
 
-        this.platformElement.style.height = this.height + "vh";
-        this.platformElement.style.width = this.width + "vw";
-        this.platformElement.style.left = this.positionX + "vw";
-        this.platformElement.style.bottom = this.positionY + "vh";
+        this.playerElement.style.height = this.height + "vh";
+        this.playerElement.style.width = this.width + "vw";
+        this.playerElement.style.left = this.positionX + "vw";
+        this.playerElement.style.bottom = this.positionY + "vh";
 
-        const board = document.getElementById("board"); 
-        board.appendChild(this.platformElement); // append to board
+        const board = document.getElementById("board");
+        board.appendChild(this.playerElement); // Append to board
+    }
+
+    jumping() {
+        console.log("jumping");
+        this.positionY++;
+        this.playerElement.style.bottom = this.positionY + "vh";
     }
 }
-
 const newPlayer = new Player();
 newPlayer.createPlayer;
 
+
+// ADDING EVENT LISTENERS: Let player jump when user clicks on the spacebar
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        newPlayer.jumping()
+    }
+});
+
+/***************************************************/
+/******************* PLATFORM **********************/
+/***************************************************/
+
 class Platform {
     constructor() {
-        this.height = 3; 
-        this.width = 15; 
+        this.height = 3;
+        this.width = 15;
         this.positionX = 0;
         this.positionY = 100 - this.height;
-        this.platformElement = null; 
+        this.platformElement = null;
 
         this.createPlatformElement();
     }
     createPlatformElement() {
         this.platformElement = document.createElement("div");
-        console.log("player element created");
 
         this.platformElement.className = "platform"; // add class
 
@@ -49,7 +68,7 @@ class Platform {
         this.platformElement.style.left = this.positionX + "vw";
         this.platformElement.style.bottom = this.positionY + "vh";
 
-        const board = document.getElementById("board"); 
+        const board = document.getElementById("board");
         board.appendChild(this.platformElement); // append to board
     }
 }
