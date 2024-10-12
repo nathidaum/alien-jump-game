4486f9a62cd5885b0c01b45d8d9dc4c289041189
nathidaum <nathidaum@gmail.com>
2024-10-12 16:08:12 +0200

    Added platform class and createPlatformElement() functionality

--- js/main.js
+++ js/main.js
@@ -4,26 +4,55 @@ class Player {
         this.width = 10; 
         this.positionX = 50 - this.width / 2; // centered starting position
         this.positionY = 0;
-        this.playerElement = null; 
+        this.platformElement = null; 
 
         this.createPlayer();
     }
     
     createPlayer() {
-        this.playerElement = document.createElement("div");
+        this.platformElement = document.createElement("div");
         console.log("player element created");
 
-        this.playerElement.id = "player"; // add ID
+        this.platformElement.id = "player"; // add ID
 
-        this.playerElement.style.height = this.height + "vh";
-        this.playerElement.style.width = this.width + "vw";
-        this.playerElement.style.left = this.positionX + "vw";
-        this.playerElement.style.bottom = this.positionY + "vh";
+        this.platformElement.style.height = this.height + "vh";
+        this.platformElement.style.width = this.width + "vw";
+        this.platformElement.style.left = this.positionX + "vw";
+        this.platformElement.style.bottom = this.positionY + "vh";
 
         const board = document.getElementById("board"); 
-        board.appendChild(this.playerElement); // append to board
+        board.appendChild(this.platformElement); // append to board
     }
 }
 
 const newPlayer = new Player();
 newPlayer.createPlayer;
+
+class Platform {
+    constructor() {
+        this.height = 3; 
+        this.width = 15; 
+        this.positionX = 0;
+        this.positionY = 100 - this.height;
+        this.platformElement = null; 
+
+        this.createPlatformElement();
+    }
+    createPlatformElement() {
+        this.platformElement = document.createElement("div");
+        console.log("player element created");
+
+        this.platformElement.className = "platform"; // add class
+
+        this.platformElement.style.height = this.height + "vh";
+        this.platformElement.style.width = this.width + "vw";
+        this.platformElement.style.left = this.positionX + "vw";
+        this.platformElement.style.bottom = this.positionY + "vh";
+
+        const board = document.getElementById("board"); 
+        board.appendChild(this.platformElement); // append to board
+    }
+}
+
+const platform1 = new Platform();
+platform1.createPlatformElement();
